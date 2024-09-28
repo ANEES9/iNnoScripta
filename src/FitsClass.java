8ba1bf588fe8abf6274b07a83ca77ac4dea317b1
Anees <mohammed.anees.69a@gmail.com>
2024-09-28 23:12:57 +0530

    first commit

---
+++ src/FitsClass.java
@@ -0,0 +1,8 @@
+
+public class FitsClass {
+	
+	public static void main(String[] args) {
+		System.out.println("yes");
+	}
+
+}
---
+++ src/FitsClass2.java
@@ -0,0 +1,8 @@
+
+public class FitsClass2 {
+	
+	public static void main(String[] args) {
+		System.out.println("yes");
+	}
+
+}
